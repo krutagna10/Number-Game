b14f67237d9fca2596b23c4076d455f6abbbdaf5
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-09-24 18:43:32 +0530

    ..

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,65 @@
-'use strict';
+const guess = document.querySelector('.guess');
+const checkButton = document.querySelector('.check');
+const againButton = document.querySelector('.again');
+const message = document.querySelector('.message');
+const scoreText = document.querySelector('.score');
+const highScoreText = document.querySelector('.highscore');
+const body = document.querySelector('body');
+
+// Generating a random number between 1 and 20
+ let randomNumber = 1 + parseInt(Math.random() * (20 - 1 + 1));
+
+// Initially the score is 20
+let score = 20;
+
+// Initially the high score is 0
+let highScore = 0;
+
+ checkButton.addEventListener('click', () => {
+    // Storing user input in a variable
+   const guessedNumber = parseInt(guess.value);
+   
+   if (!guessedNumber) {
+    message.textContent = '⛔️ No number!';
+   } else if (guessedNumber === randomNumber){
+    message.textContent = '🎉 Correct Number!';
+    body.style.backgroundColor = '#60b347';
+    score = score - 1;
+    scoreText.textContent = score;
+    if (score > highScore) {
+      highScore = score;
+      highScoreText.textContent = highScore;
+    }
+   } else if (guessedNumber > randomNumber) {
+    if (score > 1) {
+    message.textContent = '📉 Too high!';
+    score = score - 1;
+    scoreText.textContent = score;
+    } else {
+      body.style.backgroundColor = '#d62828';
+      message.textContent = '💥 You lost the game';
+      scoreText.textContent = 0;
+    }
+   } else {
+    if (score > 1) {
+    message.textContent = '📈 Too low!';
+    score = score - 1;
+    scoreText.textContent = score;
+    } else {
+      body.style.backgroundColor = '#d62828';
+      message.textContent = '💥 You lost the game';
+      scoreText.textContent = 0;
+    }
+   }
+ })
+
+//  Play again button
+againButton.addEventListener('click', () => {
+  randomNumber = 1 + parseInt(Math.random() * (20 - 1 + 1));
+  message.textContent = 'Start guessing';
+  score = 20;
+  scoreText.textContent = score;
+  body.style.backgroundColor = '#222'
+  guess.value = '';
+})
+
---
+++ README.md
@@ -0,0 +1,16 @@
+# Number Game
+
+### Screenshot
+
+![](Screenshot/Screenshot.png)
+
+### Links
+
+- Live Site URL: [https://testimonial-grid-section-10.netlify.app/]
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- CSS Grid
+
